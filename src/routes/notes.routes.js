1d264cfee3d087151696e65addb9599881ce1a59
Givaldo Neto <valdingivs@outlook.com>
2023-01-03 10:31:09 -0300

    :sparkles: feat: add delete and list notes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -8,5 +8,7 @@ const notesController = new NotesController
 
 notesRouters.post("/:user_id", notesController.create)
 notesRouters.get("/:id", notesController.show)
+notesRouters.delete("/:id", notesController.delete)
+notesRouters.get("/", notesController.index)
 
 module.exports = notesRouters
