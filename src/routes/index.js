8a0d46f7d0953649adeca1141c0c4355f57dae06
Givaldo Neto <valdingivs@outlook.com>
2023-01-03 10:12:28 -0300

    :sparkles: feat: add notes router and controller to create

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express")
 
 const usersRouters = require("./users.routes")
+const notesRouters = require("./notes.routes")
 
 const routes = Router()
 
 routes.use("/users", usersRouters)
+routes.use("/notes", notesRouters)
 
 module.exports = routes
